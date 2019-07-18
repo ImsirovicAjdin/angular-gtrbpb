9c934c9941d985f233eab86adfd91c97ace74c5f
ImsirovicAjdin <ajdika@live.com>
2019-07-18 12:52:06 +0200

    Add app-child component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
     <h1>Simpest input-output</h1>
+    <app-child></app-child>
   `
 })
 export class AppComponent  {
---
+++ src/app/appchild/app-child.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  template: `
+  <p>This is app-child</p>
+  `
+})
+export class AppChildComponent {
+  
+}
